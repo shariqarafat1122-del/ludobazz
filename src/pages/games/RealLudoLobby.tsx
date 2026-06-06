005342eb6de17ad74a0220dd8f9b23d901c02412
shariqarafat1122-del <shariqarafat1122@gmail.com>
2026-06-06 09:11:34 +0530

    Update RealLudoLobby.tsx

--- src/pages/games/RealLudoLobby.tsx
+++ src/pages/games/RealLudoLobby.tsx
@@ -8,7 +8,7 @@ import {
   joinLudoGame,
   subscribeOpenLudoGames,
   LudoGame,
-} from './RealLudo';
+} from '../../RealLudo';
 
 // ─── Utils ────────────────────────────────────────────────────────────────────
 
